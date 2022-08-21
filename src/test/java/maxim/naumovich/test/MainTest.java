2383383113580784b44115d328c5cdd17b99023c
Максим Наумович <the7fame@gmail.com>
2022-08-21 16:16:57 +0300

    add stuff

---
+++ src/test/java/maxim/naumovich/test/MainTest.java
@@ -0,0 +1,19 @@
+package maxim.naumovich.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest extends BaseTest {
+    static String pasteInField = "Hello from WebDriver";
+    static String pasteInName = "helloweb";
+
+    @Test
+    public void testCreate(){
+        page.doStuff(pasteInField, pasteInName);
+        String text = page.getText();
+        String title = page.getTitle();
+        Assert.assertEquals(pasteInField.equals(text), true);
+        Assert.assertEquals(pasteInName.equals(title), true);
+    }
+
+}
